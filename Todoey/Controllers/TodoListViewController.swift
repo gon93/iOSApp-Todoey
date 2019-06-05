0919aa596f1c7314b46ca0a7541eea993e44674b
gon <ksteve1204@gmail.com>
2019-06-05 14:15:51 -0400

    Convert to MVC design Pattern

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Finding Neemo", "Incredibles 2", "Sleeping Beauty"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+//        ["Finding Neemo", "Incredibles 2", "Sleeping Beauty", "Lion King", "Mulan", "Frozen", "Alice in Wonderland"]
+        let newItem = Item()
+        newItem.title = "Finding Neemo"
+        itemArray.append(newItem)
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        let newItem1 = Item()
+        newItem1.title = "Incredibles 2"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Sleeping Beauty"
+        itemArray.append(newItem2)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
     }
@@ -33,10 +45,17 @@ class TodoListViewController: UITableViewController {
     //print the particular data into the cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        print("cellForRowatIndexPath Called")
+        
+        //reusing the tableView cell; so if the cell does not appear on the screen, the cell goes to the bottom and be reused.
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
+        let item = itemArray[indexPath.row]
+        
         //cell text = current row of the current path
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -47,9 +66,12 @@ class TodoListViewController: UITableViewController {
         
         //print(itemArray[indexPath.row])
         
-        //checkmark or none on selected cell
-        //Ternary Operator - question ? True : False
-        (tableView.cellForRow(at: indexPath)!.accessoryType == .checkmark) ? (tableView.cellForRow(at: indexPath)!.accessoryType = .none) : (tableView.cellForRow(at: indexPath)!.accessoryType = .checkmark)
+        let item = itemArray[indexPath.row]
+        
+        item.done = !item.done
+        
+        //reload everytime cell is clicked
+        tableView.reloadData()
         
         //deselect the grey highlight
         tableView.deselectRow(at: indexPath, animated: true)
@@ -68,7 +90,10 @@ class TodoListViewController: UITableViewController {
             //what will happen once the user clicks the Add Item button on our UIAlert
 //            print(textField.text!)
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
