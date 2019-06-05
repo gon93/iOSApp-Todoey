59db8ae4305ac3bdeb579f4744f8e9fc295924ad
gon <ksteve1204@gmail.com>
2019-06-04 23:36:56 -0400

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Seong Kon Kim on 6/4/19.
+//  Copyright © 2019 Seong Kon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
